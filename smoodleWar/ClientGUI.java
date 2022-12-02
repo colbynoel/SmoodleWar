f469de18746703fc8b0843c961e6aeb2ba7983f9
bfcavin <90774954+bfcavin@users.noreply.github.com>
2022-12-01 18:23:37 -0600

    Added/Fixed Delete Panel
    Modified initial control and other classes to accommodate DeleteAccount. Will move it to lobby once that is made.

--- smoodleWar/ClientGUI.java
+++ smoodleWar/ClientGUI.java
@@ -31,6 +31,7 @@ public class ClientGUI extends JFrame {
 		InitialControl ic = new InitialControl(container, client);
 		LoginControl lc = new LoginControl(container, client);
 		CreateAccountControl cac = new CreateAccountControl(container, client);
+		DeleteAccountControl dac = new DeleteAccountControl(container, client);
 
 		// Set the client info
 		client.setLoginControl(lc);
@@ -40,13 +41,13 @@ public class ClientGUI extends JFrame {
 		JPanel view1 = new InitialPanel(ic);
 		JPanel view2 = new LoginPanel(lc);
 		JPanel view3 = new CreateAccountPanel(cac);
-		// JPanel view4 = new ContactsPanel();
+		JPanel view4 = new DeleteAccountPanel(dac);
 
 		// Add the views to the card layout container.
 		container.add(view1, "1");
 		container.add(view2, "2");
 		container.add(view3, "3");
-		// container.add(view4, "4");
+		container.add(view4, "4");
 
 		// Show the initial view in the card layout.
 		cardLayout.show(container, "1");
